906139dceb5e101a3dff8a8dd53e7f036718c152
Martin Smith <martin.smith@nokia.com>
2011-08-02 11:06:44 +0200

    qdoc3: Reverting part of the change for QTBUG-17072
    Task-number: QTBUG-17072
    
    Change-Id: I16a8b780160615065cb89b35704e107830b2aeb6
    Reviewed-on: http://codereview.qt.nokia.com/2500
    Reviewed-by: Qt Sanity Bot <qt_sanity_bot@ovi.com>
    Reviewed-by: Martin Smith <martin.smith@nokia.com>

--- tools/qdoc3/cppcodemarker.cpp
+++ tools/qdoc3/cppcodemarker.cpp
@@ -483,7 +483,7 @@ QList<Section> CppCodeMarker::sections(const InnerNode *inner,
                                         "",
                                         "public function",
                                         "public functions");
-            FastSection publicSignals(classe, "Signal Handlers", "", "signal handler", "signal handlers");
+            FastSection publicSignals(classe, "Signals", "", "signal", "signal");
 	    FastSection publicSlots(classe, "Public Slots", "", "public slot", "public slots");
 	    FastSection publicTypes(classe, "Public Types", "", "public type", "public types");
 	    FastSection publicVariables(classe,
