8a0244d3f46c1eb9904d9da7e741bb2e05ca3f3d
Alessandro Portale <alessandro.portale@qt.io>
2018-05-14 12:58:59 +0200

    Remove the qtcluster demo
    The license checker of the CI spotted deviating license headers in
    the demo. In addition, in it current state, the demo lacks any
    polishing. Let's remove it.
    
    Task-number: QTBUG-67529
    Change-Id: Ic9375deb0a889e2e7e96e874b62fbc314269e455
    Reviewed-by: Thomas Hartmann <thomas.hartmann@qt.io>
    Reviewed-by: Simon Hausmann <simon.hausmann@qt.io>

--- doc/src/snippets/qtcluster/images/convert_premultiplied_alpha.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while read -r line || [[ -n "$line" ]]; do
-   echo "Text: $line";
-   convert $line -write mpr:temp -background black -alpha Remove mpr:temp -compose Copy_Opacity -composite ../images-premultiplied-alpha/$line
-done < "$1"
