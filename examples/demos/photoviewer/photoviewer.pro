qt/qtdoc

commit 70ce5f4d3ecae010cbaefea741f67b9cc91d0be9

Move demos to qtdoc repository
These demos use several modules and moving them allows to use for
example Qt Quick Controls 2 in them.

The shared directory should be removed and the examples made
self-contained.

Change-Id: I5dc667d22388651894d7d145b65752bc5c5cf6cf
Reviewed-by: Frederik Gladhorn <frederik.gladhorn@qt.io>

examples/demos/photoviewer/photoviewer.pro
@@ -1,0 +1,19 @@
+TEMPLATE = app
+
+QT += qml quick xmlpatterns
+
+SOURCES += main.cpp
+
+lupdate_only{
+SOURCES = *.qml \
+          PhotoViewerCore/*.qml \
+          PhotoViewerCore/script/*.js
+}
+
+TRANSLATIONS += i18n/qml_fr.ts \
+                i18n/qml_de.ts
+
+RESOURCES += qml.qrc
+
+target.path = $$[QT_INSTALL_EXAMPLES]/demos/photoviewer
+INSTALLS += target

examples/demos/photoviewer/qml.qrc
@@ -1,0 +1,19 @@
+<RCC>
+    <qresource prefix="/">
+        <file>main.qml</file>
+        <file>PhotoViewerCore/AlbumDelegate.qml</file>
+        <file>PhotoViewerCore/BusyIndicator.qml</file>
+        <file>PhotoViewerCore/Button.qml</file>
+        <file>PhotoViewerCore/EditableButton.qml</file>
+        <file>PhotoViewerCore/PhotoDelegate.qml</file>
+        <file>PhotoViewerCore/ProgressBar.qml</file>
+        <file>PhotoViewerCore/RssModel.qml</file>
+        <file>PhotoViewerCore/Tag.qml</file>
+        <file>PhotoViewerCore/images/box-shadow.png</file>
+        <file>PhotoViewerCore/images/busy.png</file>
+        <file>PhotoViewerCore/images/cardboard.png</file>
+        <file>PhotoViewerCore/script/script.js</file>
+        <file>i18n/qml_fr.ts</file>
+        <file>i18n/qml_de.ts</file>
+    </qresource>
+</RCC>

commit 2f06030e8039abf0bef1316af6edc8e283a96329

photoviewer: Fix translations
It doesn't make any sense to put .ts files into resources,
because QTranslator can only parse compiled .qm files.
Therefore the application does not load any translations
at all. This error was introduced in 70ce5f4d3ec, when
the example was imported from qtdeclarative.

Anyhow, the previous version of the example in qtdeclarative
required the user to run lrelease manually, which is ugly
and made the example unfit for automatic testing.

Instead, use the new capabilities of qmake to compile
and embed translation files automatically.

Change-Id: Ic02171bfc9994ece3da658b69f0de0a27cb39cc5
Reviewed-by: Oswald Buddenhagen <oswald.buddenhagen@qt.io>

examples/demos/photoviewer/photoviewer.pro
@@ -1,10 +1,11 @@
 TEMPLATE = app
 
 QT += qml quick xmlpatterns
+CONFIG += lrelease embed_translations
 
 SOURCES += main.cpp
 
-lupdate_only{
+lupdate_only {
 SOURCES = *.qml \
           PhotoViewerCore/*.qml \
           PhotoViewerCore/script/*.js

examples/demos/photoviewer/qml.qrc
@@ -13,7 +13,5 @@
         <file>PhotoViewerCore/images/busy.png</file>
         <file>PhotoViewerCore/images/cardboard.png</file>
         <file>PhotoViewerCore/script/script.js</file>
-        <file>i18n/qml_fr.ts</file>
-        <file>i18n/qml_de.ts</file>
     </qresource>
 </RCC>