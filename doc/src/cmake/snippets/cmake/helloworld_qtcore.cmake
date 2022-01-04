ed57708f3aea07b545c11ac2f70d4f8f46af153b
Joerg Bornemann <joerg.bornemann@qt.io>
2022-01-04 16:37:32 +0100

    Doc: Add "Building a C++ console application" section
    ...on the "Getting started with CMake" page.
    
    Split "Build a C++ GUI application" into a "Building a C++ console
    application" and, based on that, "Building a C++ GUI application".
    
    Remove the mention of CMAKE_AUTORCC, because we want to discourage its
    use.  See QTBUG-87643.  In a subsequent commit, we will add a guide how
    to use resources.
    
    Task-number: QTBUG-84789
    Change-Id: I9434c88538af0d6e7c5e2c86ee86c3d697d7a576
    (cherry picked from commit 7ab524fac5593f8a390a173cbb10f6b3846d4f17)
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

---
+++ doc/src/cmake/snippets/cmake/helloworld_qtcore.cmake
@@ -0,0 +1,16 @@
+cmake_minimum_required(VERSION 3.16)
+
+project(helloworld VERSION 1.0.0 LANGUAGES CXX)
+
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+set(CMAKE_AUTOMOC ON)
+
+find_package(Qt6 REQUIRED COMPONENTS Core)
+
+add_executable(helloworld
+    main.cpp
+)
+
+target_link_libraries(helloworld PRIVATE Qt6::Core)
