9b74984d20fe5629489b466f5ef0fa9ef4326185
Joerg Bornemann <joerg.bornemann@qt.io>
2022-01-07 14:20:47 +0100

    Doc: Document how to use static plugins with CMake
    Also, update the existing QTPLUGIN example to import a plugin that is
    not imported by default.  This is closer what one would do in reality.
    
    Make the CMake examples match the qmake ones.
    
    Move text that describes CMake and qmake to the start of the section,
    and add a subsection for CMake and qmake each.
    
    Fixes: QTBUG-91413
    Change-Id: Ie67518dc8757c69f382067a7225c72ff0b512ff8
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>
    Reviewed-by: Alexey Edelev <alexey.edelev@qt.io>
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    (cherry picked from commit 456b4078252e615e35ae8c4157f1e2c645aeace0)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ doc/snippets/plugins/doc_src_plugins-howto.cmake
@@ -0,0 +1,21 @@
+#! [import_plugins]
+qt_import_plugins(myapp INCLUDE Qt::QLibInputPlugin)
+#! [import_plugins]
+
+#! [import_minimal_plugin]
+qt_import_plugins(myapp
+    INCLUDE_BY_TYPE platforms Qt::MinimalIntegrationPlugin
+)
+#! [import_minimal_plugin]
+
+#! [some_imageformat_plugins]
+qt_import_plugins(myapp
+    INCLUDE_BY_TYPE imageformats Qt::QJpegPlugin Qt::QGifPlugin
+)
+#! [some_imageformat_plugins]
+
+#! [no_imageformats_plugins]
+qt_import_plugins(myapp
+    EXCLUDE_BY_TYPE imageformats
+)
+#! [no_imageformats_plugins]
