fd0d36dbf7492dd409ce0c0c1723408bdc396768
Fabian Kosmale <fabian.kosmale@qt.io>
2023-02-21 16:20:11 +0100

    Update Building a QML application for 6.5
    - se qt_standard_project_setup instead of AUTOMOC
    - pass REQUIRED 6.5 to it
    - adjust the resource path URL accordingly
    - Group all content describing the effect of qt_add_qml_module together
    
    As a drive-by, also pass REQUIRED 6.5 to qt_standard_project_setup in
    the "Reusable QML module" section.
    
    Task-number: QTBUG-109799
    Change-Id: Ia10b0d9a3010d8653f15b626fb1828ebafade186
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>
    (cherry picked from commit c4c75c8f378e2562a19745e1eb6855480114edf5)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- doc/src/cmake/snippets/cmake/helloworld_qtqml.cmake
+++ doc/src/cmake/snippets/cmake/helloworld_qtqml.cmake
@@ -5,11 +5,11 @@ cmake_minimum_required(VERSION 3.16)
 
 project(hello VERSION 1.0 LANGUAGES CXX)
 
-set(CMAKE_AUTOMOC ON)
+find_package(Qt6 6.2 COMPONENTS Quick Gui REQUIRED)
+
+qt_standard_project_setup(REQUIRED 6.5)
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
-
-find_package(Qt6 6.2 COMPONENTS Quick Gui REQUIRED)
 
 qt_add_executable(myapp
     main.cpp
@@ -17,7 +17,6 @@ qt_add_executable(myapp
 
 qt_add_qml_module(myapp
     URI hello
-    VERSION 1.0
     QML_FILES
         main.qml
         FramedImage.qml
