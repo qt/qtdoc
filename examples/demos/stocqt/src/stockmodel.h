75ed068b04663e065f4cb6794f5c4fc47e8cc42d
Sakaria Pouke <sakaria.pouke@qt.io>
2024-01-02 08:43:37 +0200

    StocQt minor fixes
    Fixes are based on comments in "Overhaul StocQt example" patch
    
    Fixes: QTBUG-120471
    Pick-to: 6.7 6.6
    Change-Id: I5eae5aa4825f02066fae6430795c87f5ac633d42
    Reviewed-by: Tomi Korpipää <tomi.korpipaa@qt.io>

--- examples/demos/stocqt/src/stockmodel.h
+++ examples/demos/stocqt/src/stockmodel.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2023 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
 #ifndef STOCKMODEL_H
 #define STOCKMODEL_H
 
@@ -30,41 +31,40 @@ class StockModel : public QObject
 {
     Q_OBJECT
 public:
-    StockModel(QString stockId, QString name);
-    ~StockModel();
+    StockModel(const QString &stockId, const QString &name, QObject *parent = nullptr);
 
-    void addData(HistoryData *data);
-    void updateHistory(QList<HistoryData *> data);
-    void appendQuote(QuoteData *data);
+    void addData(HistoryData data);
+    void updateHistory(QList<HistoryData> const &data);
+    void appendQuote(QuoteData data);
     void resetQuote();
     void setDataIsLive(bool live);
 
-    Q_INVOKABLE int historyCount();
-    Q_INVOKABLE int quoteCount();
-    Q_INVOKABLE QString getStockId();
-    Q_INVOKABLE QString getName();
-    Q_INVOKABLE float getPrice(int index);
-    Q_INVOKABLE float getChange(int index);
-    Q_INVOKABLE float getChangePercentage(int index);
-    Q_INVOKABLE int getHigh(int index);
-    Q_INVOKABLE int getLow(int index);
-    Q_INVOKABLE int getOpen(int index);
-    Q_INVOKABLE int getClose(int index);
-    Q_INVOKABLE int getVolume(int index);
-    Q_INVOKABLE int getAvgVolume();
-    Q_INVOKABLE int daysFromNow(int index);
-    Q_INVOKABLE int indexOf(QDateTime date);
-    Q_INVOKABLE bool getDataIsLive();
-    Q_INVOKABLE long long getHistoryDate(int index, bool milliseconds = true);
-    Q_INVOKABLE long long getQuoteTime(int index, bool milliseconds = true);
+    Q_INVOKABLE int historyCount() const;
+    Q_INVOKABLE int quoteCount() const;
+    Q_INVOKABLE QString stockId() const;
+    Q_INVOKABLE QString name() const;
+    Q_INVOKABLE float price(int index) const;
+    Q_INVOKABLE float change(int index) const;
+    Q_INVOKABLE float changePercentage(int index) const;
+    Q_INVOKABLE int highPrice(int index) const;
+    Q_INVOKABLE int lowPrice(int index) const;
+    Q_INVOKABLE int openPrice(int index) const;
+    Q_INVOKABLE int closePrice(int index) const;
+    Q_INVOKABLE int volume(int index) const;
+    Q_INVOKABLE int avgVolume() const;
+    Q_INVOKABLE int daysFromNow(int index) const;
+    Q_INVOKABLE int indexOf(QDateTime date) const;
+    Q_INVOKABLE bool dataIsLive() const;
+    Q_INVOKABLE long long historyDate(int index, bool milliseconds = true) const;
+    Q_INVOKABLE long long quoteTime(int index, bool milliseconds = true) const;
 
 signals:
     void historyDataReady();
     void quoteDataReady();
 
 private:
-    QList<HistoryData *> m_historyDataList;
-    QList<QuoteData *> m_quoteDataList;
+    QList<HistoryData> m_historyDataList;
+    QList<QuoteData> m_quoteDataList;
     QString m_stockId;
     QString m_name;
     bool m_dataIsLive = false;
