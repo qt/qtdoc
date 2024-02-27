92e216b98f339eadf2ca19d7fe4e4b8c8b8f6139
Kai Köhne <kai.koehne@qt.io>
2024-02-27 08:44:09 +0100

    Fix QTP0001 warning for Showroom example
    Pick-to: 6.6
    Fixes: QTBUG-122767
    Change-Id: I65be5eb533364708284e368bef33b7066ecd30d6
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit d3d420a7c095a884efff96a4d93dd6637eba9445)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- examples/demos/FX_Material_Showroom/src/main.cpp
+++ examples/demos/FX_Material_Showroom/src/main.cpp
@@ -16,15 +16,12 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:Main/main.qml"_qs);
+    const QUrl url(u"qrc:/qt/qml/Main/main.qml"_qs);
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app,
                 [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
-
-    engine.addImportPath(QCoreApplication::applicationDirPath() + "/qml");
-    engine.addImportPath(":/");
 
     engine.load(url);
 
