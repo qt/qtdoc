86d40e28d5f5ebbc8cf86355d19880f1720a4340
Sami Shalayel <sami.shalayel@qt.io>
2024-09-19 16:28:28 +0200

    alarms: port to qt_add_qml_module
    Use qt_add_qml_module, qt_standard_project_setup, use loadFromModule(),
    rename main.qml to capital Main.qml so it can be loaded via
    loadFromModule(), rename the QML Module to Alarms as QML Modules usually
    start with an upper case letter.
    Also fix the documentation.
    
    Pick-to: 6.8
    Task-number: QTBUG-100100
    Change-Id: I8b0c7c1924c4c5e0268ca33b339ba3645bb29f7a
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>

--- examples/tutorials/alarms/main.cpp
+++ examples/tutorials/alarms/main.cpp
@@ -1,23 +1,18 @@
 // Copyright (C) 2020 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
+
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
-using namespace Qt::StringLiterals;
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:/alarms/main.qml"_s);
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app,
-                     [url](QObject *obj, const QUrl &objUrl) {
-                         if (!obj && url == objUrl)
-                             QCoreApplication::exit(-1);
-                     },
-                     Qt::QueuedConnection);
-    engine.load(url);
+    QObject::connect(
+            &engine, &QQmlApplicationEngine::objectCreationFailed, &app,
+            []() { QCoreApplication::exit(-1); }, Qt::QueuedConnection);
+    engine.loadFromModule("AlarmsModule", "Main");
 
     return app.exec();
 }
