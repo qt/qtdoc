acd89ffc23a60bcb32e4bda8ef2a00c9ceff67d1
Fabian Kosmale <fabian.kosmale@qt.io>
2025-03-18 14:30:29 +0100

    ToDo example: Be explicit about QML module dependencies
    Otherwise qmllint has a hard time figuring out where the modules are
    located.
    
    Pick-to: 6.10
    Task-number: QTBUG-138189
    Change-Id: Idde8db1302f9dbc687b90f44120a4bca64b24db2
    Reviewed-by: Sami Shalayel <sami.shalayel@qt.io>

--- examples/demos/todolist/qmlmodules.cmake
+++ examples/demos/todolist/qmlmodules.cmake
@@ -1,15 +1,17 @@
 #Copyright (C) 2023 The Qt Company Ltd.
 #SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
+add_subdirectory(imports)
+add_subdirectory(content)
+
 qt6_add_qml_module(ToDoListApp
     URI "Main"
     VERSION 1.0
-    NO_PLUGIN
+    DEPENDENCIES
+        TARGET ToDoList
+        TARGET todolist_content
     QML_FILES Main.qml
 )
-
-add_subdirectory(content)
-add_subdirectory(imports)
 
 target_link_libraries(ToDoListApp PRIVATE
     todolist_contentplugin
