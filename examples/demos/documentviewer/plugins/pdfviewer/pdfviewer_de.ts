92b812441e7ba29a5ae3f5d46d48c9a702885fe2
Kai Köhne <kai.koehne@qt.io>
2025-07-09 15:04:39 +0200

    documentviewer: Add German translations also for pdfviewer
    Remove redundant translations for <number>% and use QLocale::percent()
    instead.
    
    Change-Id: I6ddeceda6f723aae0c2a7f8338ce2d436ea434e2
    Reviewed-by: Axel Spoerl <axel.spoerl@qt.io>
    (cherry picked from commit f98040d9a4f63e577bece0c67d806b690d293673)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

---
+++ examples/demos/documentviewer/plugins/pdfviewer/pdfviewer_de.ts
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de" sourcelanguage="en">
+<context>
+    <name>PdfViewer</name>
+    <message>
+        <location filename="pdfviewer.cpp" line="83"/>
+        <location filename="pdfviewer.cpp" line="262"/>
+        <source>PDF</source>
+        <translation>PDF</translation>
+    </message>
+    <message>
+        <location filename="pdfviewer.cpp" line="112"/>
+        <location filename="pdfviewer.cpp" line="264"/>
+        <source>Zoom in</source>
+        <translation>Vergrößern</translation>
+    </message>
+    <message>
+        <location filename="pdfviewer.cpp" line="117"/>
+        <location filename="pdfviewer.cpp" line="266"/>
+        <source>Zoom out</source>
+        <translation>Verkleinern</translation>
+    </message>
+    <message>
+        <location filename="pdfviewer.cpp" line="162"/>
+        <location filename="pdfviewer.cpp" line="271"/>
+        <source>Pages</source>
+        <translation>Seiten</translation>
+    </message>
+    <message>
+        <location filename="pdfviewer.cpp" line="163"/>
+        <location filename="pdfviewer.cpp" line="274"/>
+        <source>Bookmarks</source>
+        <translation>Lesezeichen</translation>
+    </message>
+    <message>
+        <location filename="pdfviewer.cpp" line="179"/>
+        <source>Opened PDF file %1</source>
+        <translation>PDF-Datei %1 geöffnet</translation>
+    </message>
+</context>
+<context>
+    <name>ZoomSelector</name>
+    <message>
+        <location filename="zoomselector.cpp" line="13"/>
+        <location filename="zoomselector.cpp" line="38"/>
+        <source>Fit Width</source>
+        <translation>An Breite anpassen</translation>
+    </message>
+    <message>
+        <location filename="zoomselector.cpp" line="14"/>
+        <location filename="zoomselector.cpp" line="40"/>
+        <source>Fit Page</source>
+        <translation>An Seite anpassen</translation>
+    </message>
+</context>
+</TS>
