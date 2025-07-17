c3cda2a089f89e4b547b7d8958829fc6fed1dbed
Kai Köhne <kai.koehne@qt.io>
2025-07-17 09:38:04 +0200

    Doc: Improve Building a QML application walkthrough
    - Make CMakeLists.txt as close as to the console one as possible,
      to avoid misleading deviations.
    - Use same Qt minimum version in find_package and qt_standard_project_setup
    - Follow the style of the other walkthroughs to _first_ show the
      snippet, and then add the explanation.
    - Leave out the rather confusing part of the different paths for images,
      as it is unclear what the user is actually warned against.
    - Improve wording.
    
    Pick-to: 6.5
    Change-Id: I33fb986e0b7e279d467d4947d6b42c788bb65373
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    Reviewed-by: Leena Miettinen <riitta-leena.miettinen@qt.io>
    (cherry picked from commit c46d69e15d50857517935cc8d196f86bb5b12079)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>
    (cherry picked from commit be9d9d8146fdeac903edb8318774f0c4eff9cdf8)

--- doc/src/cmake/snippets/cmake/helloworld_qtqml.cmake
+++ doc/src/cmake/snippets/cmake/helloworld_qtqml.cmake
@@ -3,19 +3,20 @@
 
 cmake_minimum_required(VERSION 3.16)
 
-project(hello VERSION 1.0 LANGUAGES CXX)
+project(helloworld VERSION 1.0.0 LANGUAGES CXX)
 
-find_package(Qt6 6.2 COMPONENTS Quick Gui REQUIRED)
-
-qt_standard_project_setup(REQUIRES 6.5)
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
-qt_add_executable(myapp
+find_package(Qt6 6.5 COMPONENTS Quick REQUIRED)
+
+qt_standard_project_setup(REQUIRES 6.5)
+
+qt_add_executable(helloworld
     main.cpp
 )
 
-qt_add_qml_module(myapp
+qt_add_qml_module(helloworld
     URI hello
     QML_FILES
         main.qml
@@ -24,4 +25,4 @@ qt_add_qml_module(myapp
         img/world.png
 )
 
-target_link_libraries(myapp PRIVATE Qt6::Gui Qt6::Quick)
+target_link_libraries(helloworld PRIVATE Qt6::Quick)
