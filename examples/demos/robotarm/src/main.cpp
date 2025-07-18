e5895bd9756a1bceef25b81e3571e58a1502343e
Fabian Kosmale <fabian.kosmale@qt.io>
2025-07-18 10:43:10 +0200

    robotarm: clean up main
    Use loadFromModule, instead of load, as it's the simpler and newer API.
    That requires renaming main.qml to Main.qml, and some changes to the
    CMakeLists.txt file. That gets rid of a runtime warning about using
    relative URLs.
    
    Moreover, avoid adding an import path we don't need, and remove the
    error handling for the case where loading fails – we know it can't fail.
    
    Fixes: QTBUG-138177
    Change-Id: I01a3eac70147bf5aef6456be0b634af933daae20
    Reviewed-by: Olivier De Cannière <olivier.decanniere@qt.io>
    (cherry picked from commit f36c67b40d0f70fe83dc5376e77ba43cea21639e)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- examples/demos/robotarm/src/main.cpp
+++ examples/demos/robotarm/src/main.cpp
@@ -21,25 +21,8 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:Main/main.qml"_s);
-    QObject::connect(
-            &engine,
-            &QQmlApplicationEngine::objectCreated,
-            &app,
-            [url](QObject *obj, const QUrl &objUrl) {
-                if (!obj && url == objUrl)
-                    QCoreApplication::exit(-1);
-            },
-            Qt::QueuedConnection);
 
-    engine.addImportPath(QCoreApplication::applicationDirPath() + "/qml");
     engine.addImportPath(":/");
-
-    engine.load(url);
-
-    if (engine.rootObjects().isEmpty()) {
-        return -1;
-    }
-
+    engine.loadFromModule("Main", "Main");
     return app.exec();
 }
